c45a67f07100bbcb33a0f9294c3518806356ae3f
LauraKramp <92027219+LauraKramp@users.noreply.github.com>
2022-04-25 00:44:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 running = True
-
+#Määrab värvid mida kasutada
 MUST = (0, 0, 0)
 VALGE = (255, 255, 255)
 ORANZ = (250, 147, 30)
-
+#Joonistab akna suuruses 300x300
 screen=pygame.display.set_mode([300,300])
+#Määrab akna nime
 pygame.display.set_caption("Lumememm - Laura Kramp")
+#määrab taustavärvi
 screen.fill([0, 0, 0])
 
+#defineerib lumememme ringid ja värvid
 
 def draw_lumememm(screen, x, y):
     #Keha
@@ -21,12 +24,10 @@ def draw_lumememm(screen, x, y):
     #Nina
     pygame.draw.polygon(screen, ORANZ, [[55 + x, 30 + y], [65 + x, 30 + y], [60 + x, 45 + y]], 0)
 
-pygame.display.flip()
-
 draw_lumememm(screen,75, 30)
 pygame.display.flip()
 
-
+#võimaldab akna lahti hoida ja sulgeda ristist
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
